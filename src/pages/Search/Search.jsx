856ec5a9da053c2998431ae96273ab0e344f854e
Felipe Ickert <fvickert@gmail.com>
2018-11-11 21:15:50 -0200

    feat: added Search page

---
+++ src/pages/Search/Search.jsx
@@ -0,0 +1,85 @@
+import React, { Component } from "react";
+import SearchBar from "../../components/SearchBar";
+import BookList from "../../components/BookList";
+import Loader from "../../components/Loader";
+import * as BooksAPI from "../../BooksAPI";
+
+class Search extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      books: [],
+      hasSearched: false,
+      isLoading: false,
+      searchTerm: '',
+    };
+
+    this.onSearchHandler = this.onSearchHandler.bind(this);
+    this.onSelectOptionHandler = this.onSelectOptionHandler.bind(this);
+  }
+
+  async onSearchHandler(event) {
+    if (!event.target.value) return;
+
+    await this.setState({
+      isLoading: true,
+      searchTerm: event.target.value,
+    });
+
+    const response = await BooksAPI.search(event.target.value);
+    const books = response.error ? response.items : response;
+
+    books.forEach(book => {
+      const bookIndex = this.props.books.findIndex(b => b.id === book.id);
+
+      // if the searched book is in the gallery book, then update its shelf
+      if (bookIndex !== -1) {
+        book.shelf = this.props.books[bookIndex].shelf;
+      }
+    });
+
+    this.setState({
+      books: books,
+      isLoading: false,
+      hasSearched: true,
+    });
+  }
+
+  onSelectOptionHandler(selectedShelf, book) {
+    if (this.props.onChange) {
+      this.props.onChange(selectedShelf, book);
+    }
+  }
+
+  render() {
+    const { books, isLoading, hasSearched, searchTerm } = this.state;
+
+    return (
+      <div className="search-books">
+        <SearchBar
+          onSearch={this.onSearchHandler}
+          inputPlaceholder="Search by title or author"
+        />
+
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <div className="search-books-results">
+            {books.length ? (
+              <BookList
+                books={books}
+                onSelectOption={this.onSelectOptionHandler}
+              />
+            ) :
+            hasSearched && <p style={{ textAlign: 'center'}}>{`No books found with ${searchTerm}.`}</p>
+          }
+            
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Search;
