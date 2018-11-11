66035c6fe43f16d7501348f476548620a9860e34
Felipe Ickert <fvickert@gmail.com>
2018-11-11 21:14:49 -0200

    feat: added Loader component

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Loader = () => (
+  <div className="bookshelf_wrapper">
+    <ul className="books_list">
+      <li className="book_item first" />
+      <li className="book_item second" />
+      <li className="book_item third" />
+      <li className="book_item fourth" />
+      <li className="book_item fifth" />
+      <li className="book_item sixth" />
+    </ul>
+    <div className="shelf" />
+  </div>
+);
+
+export default Loader;
